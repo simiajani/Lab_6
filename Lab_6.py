b22ed3126d5afb423aa6d1856b6af0dfdd4e16a9
simiajani <148916768+simiajani@users.noreply.github.com>
2023-10-31 18:27:45 -0400

    Add files via upload

---
+++ Lab_6.py
@@ -0,0 +1,49 @@
+#encode funtion
+def encode(password):
+    new_password = ""
+    for num in range(password):
+        new_num = str((int(num) + 3) % 10)
+        new_password += new_num
+    return password
+
+
+def decode(password):
+    decoded_password = ""
+    for num in password:
+        og_password = int((str(num) - 3) % 10)
+        decoded_password += og_password
+    return password
+
+
+#decoding and encoding menu
+#while loop
+if __name__ == "__main__":
+
+    menu = True
+    while menu:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        print("Please enter an option:", end="")
+        option = input()
+        print("Please enter your password to encode:", end="")
+        password = input()
+
+        if option == 1:
+            #encode password
+            password = input()
+            new_password = encode(password)
+            print("Your password has been encoded and stored!")
+
+        elif option == 2:
+            #decode password
+            decoded_password = decode(new_password)
+            print("Here is your encoded password:", new_password, "Here is your decoded password:", decoded_password)
+
+        elif option == 3:
+            #quit function
+            menu = False
+            exit()
